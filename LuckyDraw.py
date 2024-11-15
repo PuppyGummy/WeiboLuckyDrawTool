8fe4572c54816b12fdc8461e4fffbcbf71e8cb61
kami <815918091@qq.com>
2024-11-14 19:11:12 -0800

    add cors

--- LuckyDraw.py
+++ LuckyDraw.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sinaweibopy3
 import random
 import os
@@ -7,6 +8,7 @@ import json
 import logging
 
 app = Flask(__name__)
+CORS(app)
 
 # 设置日志记录
 logging.basicConfig(
